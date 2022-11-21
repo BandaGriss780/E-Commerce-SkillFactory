c7b8149d1562e18c140335d2ed83c9bdfbbf3135
Camila Stefano <camilavictoriastefano@gmail.com>
2022-11-20 22:07:08 -0300

    update footer content

--- src/Cart/components/Footer.jsx
+++ src/Cart/components/Footer.jsx
@@ -1,24 +1,29 @@
 import "./styles.css";
 import avalithLogo from "./avalith-logo.svg";
+import { GoMarkGithub } from "react-icons/Go";
 
 export const Footer = () => {
   return (
     <div className="footer__wrapper">
       <div className="footer__container">
         <div className="footer__people">
+          <p>Made by</p>
           <p>
-            Made by{" "}
             <a href="https://github.com/CamiStefano" target="_blank" rel="noreferrer">
-              Camila Victoria Stefano
+              <span className="footer__people__name">Camila Victoria Stefano</span>
+              <GoMarkGithub />
             </a>{" "}
             <a href="https://github.com/dfma-89" target="_blank" rel="noreferrer">
-              Deborah Medina Alvarado
+              <span className="footer__people__name">Deborah Medina Alvarado</span>
+              <GoMarkGithub />
             </a>{" "}
             <a href="https://github.com/escobarGisele" target="_blank" rel="noreferrer">
-              Gisele Escobar
+              <span className="footer__people__name">Gisele Escobar</span>
+              <GoMarkGithub />
             </a>{" "}
             <a href="https://github.com/BandaGriss780" target="_blank" rel="noreferrer">
-              Mathias Avolio
+              <span className="footer__people__name">Mathias Avolio</span>
+              <GoMarkGithub />
             </a>
           </p>
         </div>
